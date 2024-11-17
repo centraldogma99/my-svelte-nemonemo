8133e7dcd06e27f1182cfc9c857d521fd4952e79
최준영 <jychoi@jobis.co>
2024-11-18 00:11:54 +0900

    색 변경

--- src/routes/board/Cell.svelte
+++ src/routes/board/Cell.svelte
@@ -8,7 +8,7 @@
 
 <td
 	class="border-1 h-6 w-6 select-none border border-dashed border-[#555555] text-center align-middle text-sm text-black"
-	class:bg-black={status === CellStatus.Filled}
+	class:bg-[#555555]={status === CellStatus.Filled}
 	{...tdProps}
 >
 	{status === CellStatus.X ? 'X' : ''}
